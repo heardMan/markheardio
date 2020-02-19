be9da5b7d978301e89e560ba07d73fe7e08f5b2d
heardman <markyheard@gmail.com>
2020-02-18 21:32:36 -0800

    testing recaptcha

--- scripts/app.js
+++ scripts/app.js
@@ -257,33 +257,7 @@ var App = (function (global) {
 
     }
 
-    function sendMessage(message) {
-        
-        // Example POST method implementation:
-        async function postData(url = '', data = {}) {
-            // Default options are marked with *
-            const response = await fetch(url, {
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
-                mode: 'cors', // no-cors, *cors, same-origin
-                cache: 'no-cache', // *default, no-cache, reload, force-cache, only-if-cached
-                credentials: 'same-origin', // include, *same-origin, omit
-                headers: {
-                    'Content-Type': 'application/json'
-                    // 'Content-Type': 'application/x-www-form-urlencoded',
-                },
-                redirect: 'follow', // manual, *follow, error
-                referrerPolicy: 'no-referrer', // no-referrer, *client
-                body: JSON.stringify(data) // body data type must match "Content-Type" header
-            });
-            return await response.json(); // parses JSON response into native JavaScript objects
-        }
-
-        postData('https://markheardio-contct-form-servce.herokuapp.com/', message)
-            .then((data) => {
-                console.log(data); // JSON data parsed by `response.json()` call
-            });
-
-    }
+    
 
     
 
@@ -346,12 +320,7 @@ var App = (function (global) {
 
             } else if (e.target.id === 'sendMessage') {
 
-                var message = {
-                    'name': doc.getElementById('name').value,
-                    'email': doc.getElementById('email').value,
-                    'message': doc.getElementById('message').value
-                }
-                sendMessage(message);
+                console.log('sending');
 
                 
             }
