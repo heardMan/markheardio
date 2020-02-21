cea851cd5bfb6dd3f4d7fad76ffc8a0eb9faa231
heardman <markyheard@gmail.com>
2020-02-20 21:24:10 -0800

    adding form validation

--- scripts/app.js
+++ scripts/app.js
@@ -316,7 +316,7 @@ var App = (function (global) {
             function nameChange(e){
                 console.log(e);
 
-                if (name.value.length < 1) {
+                if (name.value.length < 5) {
                     name.classList.add("red-outline");
                     var nameWarning = doc.createElement('DIV');
                     nameWarning.setAttribute('id','nameWarning');
